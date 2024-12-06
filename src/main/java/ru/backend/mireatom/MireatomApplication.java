ea67a498d09b78801dfd299cda765d2f23fbbe51
German <gnshibanov@mail.ru>
2024-12-06 11:55:58 +0300

    Project Initialization

---
+++ src/main/java/ru/backend/mireatom/MireatomApplication.java
@@ -0,0 +1,13 @@
+package ru.backend.mireatom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MireatomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MireatomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/backend/mireatom/MireatomApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.backend.mireatom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MireatomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
