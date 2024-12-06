284e4f0f16012bec2d49563aaa9d508fc5ce6707
German <gnshibanov@mail.ru>
2024-12-06 13:03:48 +0300

    Created Formula.java class

---
+++ src/main/java/ru/backend/mireatom/entities/Formula.java
@@ -0,0 +1,57 @@
+package ru.backend.mireatom.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "formulas")
+public class Formula {
+    @Id
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "latex")
+    private String latex;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "tags")
+    private String tags;
+
+    public Formula() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLatex() {
+        return latex;
+    }
+
+    public void setLatex(String latex) {
+        this.latex = latex;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+}
